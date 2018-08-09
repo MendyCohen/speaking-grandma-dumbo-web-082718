53d921ac4e8c47e8bca0c19c6c37a10e2747d79a
Mendy <mendybeltcohen@gmail.com>
2018-08-09 18:49:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,16 @@
      return "HUH?! SPEAK UP, SONNY!"
    end 
  end
+ 
+ def speak_to_grandma(string = "I LOVE YOU GRANDMA!")
+   if string != string.upcase
+     return "HUH?! SPEAK UP, SONNY!"
+     elsif string != "I LOVE YOU GRANDMA!"
+     return "NO, NOT SINCE 1938!"
+   else
+     return "I LOVE YOU GRANDMA!"
+   end 
+ end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
