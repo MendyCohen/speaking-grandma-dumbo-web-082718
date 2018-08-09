9f672608707c3c9eafdc7223b572f7e91055dfd3
Mendy <mendybeltcohen@gmail.com>
2018-08-09 18:42:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
  def speak_to_grandma(string = "I LOVE YOU GRANDMA")
    if string == string.upcase
      return "I LOVE YOU TOO PUMPKIN!"
+     elsif
+     return "NO, NOT SINCE 1938!"
    else
      return "HUH?! SPEAK UP, SONNY!"
    end 
